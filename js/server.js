f56787065db7bc0e1cc4e60113c3d3328e149764
Lachlan <lachlanterrey@yahoo.com>
2021-05-20 15:56:17 +1000

    Update

--- js/server.js
+++ js/server.js
@@ -1,6 +1,14 @@
+// https://zellwk.com/blog/crud-express-mongodb/
+
+
+
 const express = require('express');
 const app = express();
 
 app.listen(3000, function(){
     console.log('using port 3000');
-})
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
